a15eaacd23a608ceea99391385e1cb71b49b2104
Marcap00 <cappellatomarco@yahoo.com>
2024-11-04 17:31:03 +0100

    created routes and pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware()->prefix('admin.projects')->name('admin/projects')->group(function () {
+    Route::get('/', [AdminProjectController::class, 'index'])->name('index');
+    Route::post('/', [AdminProjectController::class, 'store'])->name('store');
+    Route::get('/create', [AdminProjectController::class, 'create'])->name('create');
+    Route::get('/{id}', [AdminProjectController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [AdminProjectController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [AdminProjectController::class, 'update'])->name('update');
+    Route::delete('/{id}', [AdminProjectController::class, 'destroy'])->name('destroy');
+    Route::delete('/{id}/permanent-delete', [AdminProjectController::class, 'permanentDestroy'])->name('permanent-destroy');
+    Route::get('/{id}/restore', [AdminProjectController::class, 'restore'])->name('restore');
+});
